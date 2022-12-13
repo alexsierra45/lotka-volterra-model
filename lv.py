8b65860ab0fbacd6c497673b6fe7c389458295ae
asciber45 <alex.sierra@estudiantes.matcom.uh.cu>
2022-12-13 16:25:56 -0500

    lotka-volterra trajectory pic

---
+++ lv.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+
+# LV model differential equations.
+def deriv(y, t, r1, r2, b1, b2):
+    R, F = y
+    dRdt = r1 * R - b1 * R * F
+    dFdt = -r2 * F + b2 * R * F
+    return dRdt, dFdt
+
+# Initial number of preys and predators, R0 and F0.
+R0, F0 = 40, 9
+# Initialy there are a total population of 49 animals
+r1 = 0.1 # Instantaneous rate of increase of preys in the absence of predators
+b1 = 0.02 # Susceptibility of preys to being hunted
+r2 = 0.3 # Instantaneous rate of decline of predators in the case of absence of preys
+b2 = 0.01 # Ability of predators to predation
+t = np.linspace(0, 200, 200)
+
+# Initial conditions vector
+y0 = R0, F0
+# Integrate the LV equations over the time grid, t.
+ret = odeint(deriv, y0, t, args=(r1, r2, b1, b2))
+R, F = ret.T
+
+x_max = np.max(R) * 1.05
+y_max = np.max(F) * 1.05
+x = np.linspace(0, x_max, 25)
+y = np.linspace(0, y_max, 25)
+xx, yy = np.meshgrid(x, y)
+uu, vv = deriv((xx, yy), 0, r1, r2, b1, b2)
+norm = np.sqrt(uu**2 + vv**2)
+uu = uu / norm
+vv = vv / norm
+plt.quiver(xx, yy, uu, vv, norm, cmap=plt.cm.gray)
+plt.plot(R, F)
+plt.savefig('trayectorias')
